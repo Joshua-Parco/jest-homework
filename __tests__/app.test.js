dc1232571f3333d1899e19f262347db166e47202
Joshua Parco <jacparco@gmail.com>
2020-12-02 15:50:47 -0700

    initial commit, first 5 functions

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,12 +1,49 @@
 import {
   // methods,
   // methods,
-  originalArray
+  originalArray,
+  doubleArray,
+  evensOnlyArray,
+  arraySum,
+  allNumbersGreaterThanZero,
+
 } from '../app'
 
 // hint of look in before_each before_all
 describe('originalArray Test', ()=> {
   test('originalArray should return it self', ()=>{
-    expect(originalArray([1,2,3,4])).toEqual([1,2,3,4])
-  })
-})
+    expect(originalArray([1,2,3,4])).toEqual([1,2,3,4]);
+  });
+});
+
+describe('doubleArray Test', () => {
+  test("doubleArray should return each number element doubled", () => {
+    let numberArray = [1, 2, 3, 4];
+    let result = [2, 4, 6, 8];
+    expect(doubleArray(numberArray)).toEqual(result);
+  });
+});
+
+describe('evensOnlyArray', () => {
+  test("evensOnlyArray should return only even elements from array", () => {
+    let numberArray = [1, 2, 3, 4];
+    let result = [2, 4];
+    expect(evensOnlyArray(numberArray)).toEqual(result);
+  });
+});
+
+describe('arraySum', () => {
+  test("arraySum should return the sum on the elements in array", () => {
+    let numberArray = [1, 2, 3, 4];
+    let result = 10;
+    expect(arraySum(numberArray)).toEqual(result);
+  });
+});
+
+describe('allNumbersGreaterThanZero', () => {
+  test("allNumbersGreaterThanZero should return true if all elements are greater than 0", () => {
+    let numberArray = [1, 2, 3, 4];
+    let result = true;
+    expect(allNumbersGreaterThanZero(numberArray)).toEqual(result);
+  });
+});
--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 export const originalArray = (arr) =>{
-  return arr
-}
+  return arr;
+};
+
+export const doubleArray = (numberArray) => {
+  return numberArray.map((num) => num * 2);
+};
+
+export const evensOnlyArray = (numberArray) => {
+  return numberArray.filter((num) => num % 2 == 0);
+};
+
+export const arraySum = (numberArray) => {
+  return numberArray.reduce((total, num) => total + num);
+};
+
+export const allNumbersGreaterThanZero = (numberArray) => {
+  return numberArray.every((num) => num > 0 );
+};
+
+export const someNumbersAreOdd = (numberArray) => {
+  return numberArray.some((num) => num % 2 == 0);
+};
